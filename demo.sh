48770aaf5294b4bd94adfbfcaa521e06dd59e3f0
telzhou618 <telzhou618@qq.com>
2020-05-16 14:31:15 +0800

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -23,10 +23,10 @@ else
 fi
 
 # meta server url
-config_server_url=http://localhost:8080
-admin_server_url=http://localhost:8090
+config_server_url=http://119.45.136.121:8080
+admin_server_url=http://119.45.136.121:8090
 eureka_service_url=$config_server_url/eureka/
-portal_url=http://localhost:8070
+portal_url=http://119.45.136.121:8070
 
 # JAVA OPTS
 BASE_JAVA_OPTS="-Denv=dev"
